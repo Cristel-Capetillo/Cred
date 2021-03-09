ef114a152fdfa35d2eaaaa8b83d334214c9666af
Axolotl-Cris <cristel.capetillo@outlook.com>
2021-03-09 16:58:04 +0100

    Adds dressup scene

---
+++ Assets/Game/Scripts/Clothing/ClothingManagerTest.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace Cred.Scripts.Clothing {
+    public class ClothingManagerTest : MonoBehaviour {
+
+        public void SelectPants() {
+            Debug.Log("Pants selected!");
+        }
+
+        public void SelectShirts() {
+            Debug.Log("Shirts selected!");
+        }
+        
+    }
+}
