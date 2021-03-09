42852e5dc3127f3b536b2cdc47d98112b70e208d
Axolotl-Cris <cristel.capetillo@outlook.com>
2021-03-09 17:21:32 +0100

    Updates ClothingManagerTest.cs

--- Assets/Game/Scripts/Clothing/ClothingManagerTest.cs
+++ Assets/Game/Scripts/Clothing/ClothingManagerTest.cs
@@ -1,7 +1,15 @@
+using System;
 using UnityEngine;
 
 namespace Cred.Scripts.Clothing {
     public class ClothingManagerTest : MonoBehaviour {
+
+        //TODO: Use the event broker to update the body parts
+        //TODO: Instead of colour, change the mesh or sprite or textures
+        
+        [SerializeField] Transform torso;
+        [SerializeField] Transform leftLeg;
+        [SerializeField] Transform rightLeg;
 
         public void SelectPants() {
             Debug.Log("Pants selected!");
@@ -9,7 +17,14 @@ namespace Cred.Scripts.Clothing {
 
         public void SelectShirts() {
             Debug.Log("Shirts selected!");
+            Debug.Log(torso);
+            var torsoMesh = torso.GetComponent<MeshRenderer>();
+            torsoMesh.material.color = Color.green;
         }
-        
+
+        void Start() {
+            throw new NotImplementedException();
+        }
+
     }
 }
