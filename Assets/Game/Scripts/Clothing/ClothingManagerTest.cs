be0b03c95a4672bd73887fb2e677972504f7a922
Axolotl-Cris <cristel.capetillo@outlook.com>
2021-03-09 17:39:46 +0100

    Adds pantsButton functionality on ClothingManagerTest.cs

--- Assets/Game/Scripts/Clothing/ClothingManagerTest.cs
+++ Assets/Game/Scripts/Clothing/ClothingManagerTest.cs
@@ -6,25 +6,24 @@ namespace Cred.Scripts.Clothing {
 
         //TODO: Use the event broker to update the body parts
         //TODO: Instead of colour, change the mesh or sprite or textures
-        
+        //TODO: Add a reset button to go back to default
+
         [SerializeField] Transform torso;
         [SerializeField] Transform leftLeg;
         [SerializeField] Transform rightLeg;
 
         public void SelectPants() {
             Debug.Log("Pants selected!");
+            var leftMesh = leftLeg.GetComponent<MeshRenderer>();
+            leftMesh.material.color = Color.blue;
+            var rightMesh = rightLeg.GetComponent<MeshRenderer>();
+            rightMesh.material.color = Color.blue;
         }
 
         public void SelectShirts() {
             Debug.Log("Shirts selected!");
-            Debug.Log(torso);
             var torsoMesh = torso.GetComponent<MeshRenderer>();
             torsoMesh.material.color = Color.green;
         }
-
-        void Start() {
-            throw new NotImplementedException();
-        }
-
     }
 }
