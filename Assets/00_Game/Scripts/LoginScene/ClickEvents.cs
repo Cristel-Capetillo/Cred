a79f6072151f9dcbb7457a44d3b12bd66133c765
Emil Huzell <65963728+EmilHuzell@users.noreply.github.com>
2021-03-11 13:24:06 +0100

    updates loginscene style

--- Assets/00_Game/Scripts/LoginScene/ClickEvents.cs
+++ Assets/00_Game/Scripts/LoginScene/ClickEvents.cs
@@ -12,6 +12,8 @@ namespace Cred
         public InputField UsernameText;
         public InputField PasswordText;
         public GameObject SubmitButton;
+        public GameObject LoginToggle;
+        public GameObject SignUpToggle;
         // Start is called before the first frame update
         void Start() {
             SubmitButton.GetComponent<Button>().onClick.AddListener(LoginClick);
@@ -42,15 +44,28 @@ namespace Cred
         }
 
         public void LoginToggleButton() {
+            //change submit button style
             SubmitButton.transform.Find("Text").GetComponent<Text>().text = "Login";
+            SubmitButton.GetComponent<Image>().color =  new Color(1f,0.19215686274f,0.6862745098f, 1);
+            
+            //change submitbutton functionality
             SubmitButton.GetComponent<Button>().onClick.RemoveAllListeners();
             SubmitButton.GetComponent<Button>().onClick.AddListener(LoginClick);
             
+            //change toggle buttons style
+            LoginToggle.GetComponent<RectTransform>().localScale = new Vector3(0.2f,0.39f, 0.5982905f);
+            SignUpToggle.GetComponent<RectTransform>().localScale = new Vector3(0.2f,0.3f, 0.5982905f);
+
         }
         public void SignUpToggleButton() {
             SubmitButton.transform.Find("Text").GetComponent<Text>().text = "Sign Up";
+            SubmitButton.GetComponent<Image>().color =  new Color(0.16078431372f,0.72549019607f,0.98039215686f, 1);
+            
             SubmitButton.GetComponent<Button>().onClick.RemoveAllListeners();
             SubmitButton.GetComponent<Button>().onClick.AddListener(SignUpClick);
+            
+            LoginToggle.GetComponent<RectTransform>().localScale = new Vector3(0.2f,0.3f, 0.5982905f);
+            SignUpToggle.GetComponent<RectTransform>().localScale = new Vector3(0.2f,0.39f, 0.5982905f);
         }
         
     }
