8f4e78df64c4884096cb996256f3722367a022ec
Michael Van Der Poll <michaelvdp@gmail.com>
2021-03-16 15:14:21 +0100

    Integrates adWatch Timer from firebase saved time

--- Assets/00_Game/Scripts/Utilities/Time/TimeHandler.cs
+++ Assets/00_Game/Scripts/Utilities/Time/TimeHandler.cs
@@ -2,7 +2,7 @@
 
 namespace Utilities.Time {
     public class TimeHandler {
-        ITimeProvider timeProvider;
+        readonly ITimeProvider timeProvider;
 
         public TimeHandler() {
             timeProvider = new RealTime();
@@ -17,15 +17,5 @@ namespace Utilities.Time {
         public DateTime GetTime() {
             return timeProvider.GetTime();
         }
-
-        /// <summary>
-        /// Returns if a specified amount of time has elapsed between time1 and time2
-        /// </summary>
-        // public bool EnoughTimePassed(int timeReq, DateTime time1, DateTime time2) {
-        //     var timePassed = timeProvider.TimeDifference(time1, time2);
-        //     Debug.Log("Time Passed: " + timePassed);
-        //     var tmp = timePassed >= timeReq;
-        //     return tmp;
-        // }
     }
 }
