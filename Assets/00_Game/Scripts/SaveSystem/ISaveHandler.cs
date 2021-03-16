c62736313d032f3ea24029455cc8b01ce61e0542
Alex <Alex.rynkowski90@gmail.com>
2021-03-16 18:00:01 +0100

    adds a preview of how to save lists and dictionaries

--- Assets/00_Game/Scripts/SaveSystem/ISaveHandler.cs
+++ Assets/00_Game/Scripts/SaveSystem/ISaveHandler.cs
@@ -3,7 +3,7 @@
 namespace SaveSystem {
     public interface ISaveHandler {
         public void Authenticate(string saveID);
-        public void Save(object saveObj);
+        public void Save<T>(T saveObj);
         public Task<T> Load<T>(string loadID);
     }
 }
