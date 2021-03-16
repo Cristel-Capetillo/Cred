7435d8d96d2d456da66d42f0b5d29be680333167
Adam Tapper <adam@tapper.one>
2021-03-16 11:15:17 +0100

    adds Stylepoints to Wearable and DressupScene

--- Assets/00_Game/Scripts/Clothing/Rarity.cs
+++ Assets/00_Game/Scripts/Clothing/Rarity.cs
@@ -4,6 +4,8 @@ namespace Clothing {
     [CreateAssetMenu(menuName = "ScriptableObjects/Rarity")]
     public class Rarity : ScriptableObject{
         [SerializeField] int value;
+        [SerializeField] int maxValue;
         public int Value => value;
+        public int MaxValue => maxValue;
     }
 }
