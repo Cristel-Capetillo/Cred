6ef416dcc45f54cc49dceb2e38f799331b85f935
Michael Van Der Poll <michaelvdp@gmail.com>
2021-03-17 10:23:03 +0100

    Adds script to DoubleRewards Button prefab to add onclick event

---
+++ Assets/00_Game/Scripts/HUD/DoubleRewardsButton.cs
@@ -0,0 +1,14 @@
+﻿using Ads;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HUD {
+    public class DoubleRewardsButton : MonoBehaviour {
+        Button button;
+
+        void Start() {
+            button = GetComponent<Button>();
+            button.onClick.AddListener(() => FindObjectOfType<AdsManager>().ShowRewardedAd(true));
+        }
+    }
+}
