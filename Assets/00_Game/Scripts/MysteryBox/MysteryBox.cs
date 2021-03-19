f0611c284120f63d233e3881b48b9656903e3987
Michael Van Der Poll <michaelvdp@gmail.com>
2021-03-19 15:56:32 +0100

    Sets up MysteryBox for later when it can award rewards, and load models to show reward gained.

--- Assets/00_Game/Scripts/MysteryBox/MysteryBox.cs
+++ Assets/00_Game/Scripts/MysteryBox/MysteryBox.cs
@@ -1,18 +1,40 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 namespace MysteryBox {
     public class MysteryBox : MonoBehaviour {
         
-        LootTable lootTable;
+        [SerializeField] float spawnRewardAfterDelay = 1.5f;
         [SerializeField] float destroyDelay = 5f;
 
+        LootTable lootTable;
+        
         void Start() {
-            Destroy(gameObject, destroyDelay);
+            StartCoroutine(StartRewardProcess(spawnRewardAfterDelay));
         }
         
         public void AssignLootTable(LootTable lootTable) {
             this.lootTable = lootTable;
         }
+
+        IEnumerator StartRewardProcess(float delay) {
+            yield return new WaitForSeconds(delay);
+            //ShowReward();
+            Destroy(gameObject, destroyDelay);
+        }
+        
+        
+        void ShowReward() {
+            //play animation and spawn visual Representation of loot acquired, perhaps play sound
+            Instantiate(lootTable.Reward()); 
+        }
+        void OnDestroy() {
+            StopCoroutine(nameof(StartRewardProcess));
+        }
+
+        
+        
         // public void StartAnimation() {
         //     animator.SetTrigger("OpenLootBox");
         // }
