925d1bd0ca0f69174a9a12aaa1f13f354e983f17
Alex <Alex.rynkowski90@gmail.com>
2021-03-19 11:55:52 +0100

    project settings

--- Assets/00_Game/Scripts/Login/AutoLogin.cs
+++ Assets/00_Game/Scripts/Login/AutoLogin.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Threading.Tasks;
 using Firebase.Auth;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,15 +8,18 @@ namespace Login {
     public class AutoLogin : MonoBehaviour
     {
         private FirebaseAuth auth;
-        
-        IEnumerator Start()
-        {
-            yield return auth = FirebaseAuth.DefaultInstance;
-   
-            //this seems to be required to NOT create new users for each login
-            var dummyUserID = auth.CurrentUser.UserId;
+        Task tmp;
 
-            yield return auth.SignInAnonymouslyAsync().ContinueWith(task => {
+        string deviceID;
+        async void Start() {
+            deviceID = SystemInfo.deviceUniqueIdentifier;
+            print($"Device ID: {deviceID}");
+            auth = FirebaseAuth.DefaultInstance;
+            //this seems to be required to NOT create new users for each login
+            //var dummy = auth.CurrentUser.UserId;
+            await auth.SignInAnonymouslyAsync();
+
+            await auth.SignInAnonymouslyAsync().ContinueWith(task => {
                 if (task.IsCanceled) {
                     Debug.LogError("SignInAnonymouslyAsync was canceled.");
                     return;
@@ -24,16 +28,18 @@ namespace Login {
                     Debug.LogError("SignInAnonymouslyAsync encountered an error: " + task.Exception);
                     return;
                 }
+                
+                print($"result is: {task.Result.UserId}");
 
                 var newUser = task.Result;
                 Debug.LogFormat("User signed in successfully: {0} ({1})",
                     newUser.DisplayName, newUser.UserId);
                 
+                
                 Debug.Log("username after sign in: " + auth.CurrentUser.UserId);
            
 
-            }).IsCompleted;
-            
+            });
             SceneManager.LoadScene("MainScene");
         }
     }
