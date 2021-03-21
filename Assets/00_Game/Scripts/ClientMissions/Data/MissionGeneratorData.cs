a63b2d24649b563a9c3f8560a7b76a89930b3fba
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2021-03-21 21:59:28 +0100

    Adds more info and attributes for generatorMissionData

--- Assets/00_Game/Scripts/ClientMissions/Data/MissionGeneratorData.cs
+++ Assets/00_Game/Scripts/ClientMissions/Data/MissionGeneratorData.cs
@@ -6,25 +6,30 @@ namespace ClientMissions.Data{
     [CreateAssetMenu(fileName = "ScriptableObject/MissionData/GeneratorData")]
     public class MissionGeneratorData : ScriptableObject{
         [SerializeField] List<MissionDifficulty> missionDifficulties;
+        [Header("Values based on missionDifficulties index")]
+        [SerializeField,Range(0, 2)]List<int> easyModeMissionCycle = new List<int>(10);
+        [SerializeField,Range(0, 2)]List<int> mediumModeMissionCycle = new List<int>(10);
+        [SerializeField,Range(0, 2)]List<int> hardModeMissionCycle = new List<int>(10);
+        [Header("Values based on followers:")]
+        [SerializeField]int easyModeEndValue = 200;
+        [SerializeField]int hardModeStartValue = 1000;
         [SerializeField] List<ColorData> colors;
         [SerializeField] List<Rarity> rarities;
         [SerializeField] List<ClothingType> clothingTypes;
         [SerializeField] List<ClientTestData> clientData = new List<ClientTestData>();
-        [SerializeField, Header("Values based on followers:")]int easyModeEndValue = 200;
-        [SerializeField]int hardModeStartValue = 1000;
-        [SerializeField]List<int> easyModeMissionCycle = new List<int>(10);
-        [SerializeField]List<int> mediumModeMissionCycle = new List<int>(10);
-        [SerializeField]List<int> hardModeMissionCycle = new List<int>(10);
+        
+        
         public List<MissionDifficulty> MissionDifficulties => missionDifficulties;
+        public List<int> EasyModeMissionCycle => easyModeMissionCycle;
+        public List<int> MediumModeMissionCycle => mediumModeMissionCycle;
+        public List<int> HardModeMissionCycle => hardModeMissionCycle;
         public List<ColorData> Colors => colors;
         public List<Rarity> Rarities => rarities;
         public List<ClothingType> ClothingTypes => clothingTypes;
         public List<ClientTestData> ClientData => clientData;
         public int EasyModeEndValue => easyModeEndValue;
         public int HardModeStartValue => hardModeStartValue;
-        public List<int> EasyModeMissionCycle => easyModeMissionCycle;
-        public List<int> MediumModeMissionCycle => mediumModeMissionCycle;
-        public List<int> HardModeMissionCycle => hardModeMissionCycle;
+        
         
     }
 }
