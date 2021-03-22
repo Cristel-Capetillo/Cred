1b168cc348e489fbb6ca5cfe98835e13387d1814
Axolotl-Cris <cristel.capetillo@outlook.com>
2021-03-22 16:08:51 +0100

    Adds a DonatingScene and some scripts

---
+++ Assets/00_Game/Scripts/HUD/Donate/DonationHandler.cs
@@ -0,0 +1,12 @@
+using Clothing;
+using UnityEngine;
+namespace HUD.Donate {
+    public class DonationHandler : MonoBehaviour {
+        
+        
+        void CheckForDuplicate() {
+           
+        }
+        
+    }
+}
