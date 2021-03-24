323fc95e4357bc49466a56862b33b9c3f0d2bd64
Alex <Alex.rynkowski90@gmail.com>
2021-03-24 13:17:45 +0100

    popup window changes for upcycle

---
+++ Assets/00_Game/Scripts/Clothing/Upgrade/EventAddToUpgradeSlot.cs
@@ -0,0 +1,13 @@
+﻿using HUD.Clothing;
+
+namespace Clothing.Upgrade {
+    public class EventAddToUpgradeSlot {
+        public readonly CombinedWearables combinedWearable;
+        public readonly InventoryButtonScript inventoryButtonScript;
+
+        public EventAddToUpgradeSlot(CombinedWearables combinedWearable, AssignCombinedWearableToUpCycle assignCombinedWearableToUpCycle) {
+            this.combinedWearable = combinedWearable;
+            this.inventoryButtonScript = inventoryButtonScript;
+        }
+    }
+}
---
+++ Assets/00_Game/Scripts/HUD/Clothing/PopupWindowUpCycleDonate.cs
@@ -0,0 +1,28 @@
+﻿using Clothing.Upgrade;
+using UnityEngine;
+using Utilities;
+
+namespace HUD.Clothing {
+    public class PopupWindowUpCycleDonate : MonoBehaviour {
+        UpcycleWearables upCycleWearables;
+
+        [SerializeField] GameObject[] popUpWindows;
+
+        void Start() {
+            upCycleWearables = GetComponent<UpcycleWearables>();
+        }
+
+        public void OnClickEnterPopUpWindow(GameObject popupWindow) {
+            foreach (var window in popUpWindows) {
+                if (popupWindow == window) {
+                    window.SetActive(!window.activeSelf);
+                    continue;
+                }
+
+                window.SetActive(false);
+            }
+
+            EventBroker.Instance().SendMessage(new EventTogglePopWindow(popupWindow.activeSelf));
+        }
+    }
+}
