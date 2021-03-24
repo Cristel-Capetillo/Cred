3dffe5396951bf1e81f2bca63ab50860ba2f0445
Joel Ring <joel.ring@gmail.com>
2021-03-24 16:55:45 +0100

    cleans up code

--- Assets/00_Game/Scripts/Clothing/DressUp/ClothingManager.cs
+++ Assets/00_Game/Scripts/Clothing/DressUp/ClothingManager.cs
@@ -6,7 +6,6 @@ using Utilities;
 
 namespace Clothing.DressUp {
     public class ClothingManager : MonoBehaviour {
-        //TODO: put all body parts in a list. make sure the bodyparts have the same names as in code
         [SerializeField] List<GameObject> bodyParts;
         [SerializeField] List<GameObject> clothingRarities;
         
@@ -15,6 +14,9 @@ namespace Clothing.DressUp {
             EventBroker.Instance().SubscribeMessage<EventClothesChanged>(UpdateClothes);
             EventBroker.Instance().SubscribeMessage<RemoveAllClothes>(RemoveClothes);
             
+            GetAllBodyParts();
+        }
+        void GetAllBodyParts() {
             foreach (var rarity in clothingRarities) {
                 foreach (Transform clothingType in rarity.transform) {
                     foreach (Transform bodyPart in clothingType) {
@@ -23,9 +25,7 @@ namespace Clothing.DressUp {
                     }
                 }
             }
-           
         }
-
         void RemoveClothes(RemoveAllClothes obj) {
             foreach (var rarity in clothingRarities) {
                 foreach (Transform child in rarity.transform) {
@@ -46,21 +46,26 @@ namespace Clothing.DressUp {
 
         void UpdateClothes(EventClothesChanged eventClothesChanged) {
             
-            //TODO: When clicking on clothes already equipped, they should be removed.
-            bool clothesRemoved = CompareAndRemoveSelectedClothes(eventClothesChanged);
+            bool clothesRemoved = CompareAndRemoveSelectedClothes(eventClothesChanged.CombinedWearables);
 
             if (clothesRemoved) {
                 //update last known clothes (null or empty)
+                var clothingType = eventClothesChanged.CombinedWearables.clothingType.name;
+                FieldInfo clothesProperty = typeof(LastKnownClothes).GetField(clothingType);
+                if (clothesProperty == null) {
+                    throw  new Exception("can't find the clothing type in last known clothes!");
+                }
+                CombinedWearables lastKnownClothingItem = clothesProperty.GetValue(lastKnownClothes) as CombinedWearables;
                 
                 //return;
             }
             
             //TODO: disable/enable objects based on the rarity of the new clothes
-            EnableOrDisableCategory(eventClothesChanged);
+            EnableOrDisableCategory(eventClothesChanged.CombinedWearables);
             
   
             //TODO: put on the actual new clothes
-            DressBodyParts(eventClothesChanged);
+            DressBodyParts(eventClothesChanged.CombinedWearables);
             
             
             //TODO: Update last known clothing item
@@ -69,7 +74,7 @@ namespace Clothing.DressUp {
             
         }
 
-        void DressBodyParts(EventClothesChanged eventClothesChanged) {
+        void DressBodyParts(CombinedWearables combinedWearable) {
             //put on the different clothing parts on the different body parts
             //TODO: change the switch to a nested foreach() too loop through all received clothes and match with the corresponding body parts
             //TODO: make sure the different limbs have the same name as the wearables body parts
@@ -83,42 +88,29 @@ namespace Clothing.DressUp {
                 
             }*/
             //TODO: When putting on a jacket -> Deactivate shirt sleeves
-            if (eventClothesChanged.CombinedWearables.clothingType.name == "Jackets") {
+            if (combinedWearable.clothingType.name == "Jackets") {
                 //deactivate the correct shirt sleeves
             }
         }
 
-        void EnableOrDisableCategory(EventClothesChanged eventClothesChanged) {
+        void EnableOrDisableCategory(CombinedWearables combinedWearable) {
             //enable/disable the objects, based on the incoming new clothes
             //for example: we get basic pants
             //disable all pants
             //enable basic pants
         }
 
-        bool CompareAndRemoveSelectedClothes(EventClothesChanged eventClothesChanged) {
-            //TODO: When clicking on clothes already equipped, they should be removed.
-            //possibly calculate item ID and compare to the one equipped
-            //or just compare the objects
-
-            if (eventClothesChanged.CombinedWearables == null) {
+        bool CompareAndRemoveSelectedClothes(CombinedWearables combinedWearable) {
+            if (combinedWearable == null) {
                 throw new Exception("no combined wearables found");
             }
-            var clothingType = eventClothesChanged.CombinedWearables.clothingType.name;
-            FieldInfo clothesProperty = typeof(LastKnownClothes).GetField(clothingType);
-            if (clothesProperty == null) {
-                throw  new Exception("can't find the clothing type in last known clothes!");
-            }
-            CombinedWearables lastKnownClothingItem = clothesProperty.GetValue(lastKnownClothes) as CombinedWearables;
-
-            if (lastKnownClothingItem == eventClothesChanged.CombinedWearables) {
-                //remove this piece of clothes
-                var rarity = clothingRarities.Find(i => i.name == eventClothesChanged.CombinedWearables.rarity.name);
-                var clothTransform = rarity.transform.Find(eventClothesChanged.CombinedWearables.clothingType.name);
-                clothTransform.gameObject.SetActive(false);
+            
+            if (GetLastKnownItemOfType(combinedWearable.clothingType.name) == combinedWearable) {
+                RemoveClothesPiece(combinedWearable.rarity.name, combinedWearable.clothingType.name);
                 
                 //TODO: Special case: When removing a jacket -> activate shirt sleeves
                 //TODO: but first check if a shirt is currently equipped
-                if (eventClothesChanged.CombinedWearables.clothingType.name == "Jackets") {
+                if (combinedWearable.clothingType.name == "Jackets") {
                     var shirtRarity = lastKnownClothes.Shirts.rarity.name;
                     //TODO : complete this
                 }
@@ -126,6 +118,20 @@ namespace Clothing.DressUp {
                 return true;
             }
             return false;
+        }
+
+        void RemoveClothesPiece(string rarityName, string clothingTypeName) {
+            var rarity = clothingRarities.Find(i => i.name == rarityName);
+            var clothTransform = rarity.transform.Find(clothingTypeName);
+            clothTransform.gameObject.SetActive(false);
+        }
+
+        CombinedWearables GetLastKnownItemOfType(string clothingTypeName) {
+            var clothesProperty = typeof(LastKnownClothes).GetField(clothingTypeName);
+            if (clothesProperty == null) {
+                throw  new Exception("can't find the clothing type in last known clothes!");
+            }
+            return clothesProperty.GetValue(lastKnownClothes) as CombinedWearables;
         }
 
         void OnDestroy() {
