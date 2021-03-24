83a50810f514faade747d47b44eefabb1610da31
Michael Van Der Poll <michaelvdp@gmail.com>
2021-03-24 13:28:23 +0100

    tweaks to the API for Timestamp, temporarily disables MysteryBox reward Spawning

--- Assets/00_Game/Scripts/HUD/MysteryBox/MysteryBoxButton.cs
+++ Assets/00_Game/Scripts/HUD/MysteryBox/MysteryBoxButton.cs
@@ -1,10 +1,18 @@
+using System;
 using UnityEngine;
+using Utilities.Time;
 
 namespace HUD.MysteryBox {
     public class MysteryBoxButton : MonoBehaviour {
         public GameObject selectorMenuPrefab;
         public RectTransform parentCanvas;
-        
+
+        TimeStamp freeMysteryBox;
+
+        void Start() {
+            freeMysteryBox = new TimeStamp("freeMysteryBox");
+        }
+
         public void SpawnLootBoxSelectionMenu() {
             var instance = Instantiate(selectorMenuPrefab, parentCanvas);
             //instance.GetComponent<SelectorMenu>().GenerateMysteryBoxes(6);
--- Assets/00_Game/Scripts/Utilities/Time/TimeStamp.cs
+++ Assets/00_Game/Scripts/Utilities/Time/TimeStamp.cs
@@ -5,40 +5,53 @@ using UnityEngine;
 namespace Utilities.Time {
     public class TimeStamp : ISavable<string> {
         
-        readonly DateTime epoch = new DateTime(1970, 01, 01, 0, 0, 0);
-        
+        public static readonly DateTime epoch = new DateTime(1970, 01, 01, 0, 0, 0);
+
         SaveHandler saveHandler;
-        
+        string ID;
+
         public DateTime Time { get; private set; }
 
+        /// <summary>
+        /// Loads the timeStamp from Online Save System with the given parameter name.
+        /// Call Save() to save to SaveHandler.
+        /// </summary>
         public TimeStamp(string saveID) {
             saveHandler = new SaveHandler(saveID);
+            ID = saveID;
             saveHandler.Load(this);
         }
 
+        /// <summary>
+        /// Create a new TimeStamp with the provided parameter name, and assign it a DateTime
+        /// this will then save to the SaveHandler.
+        /// </summary>
         public TimeStamp(string saveID, DateTime pointInTime) {
             saveHandler = new SaveHandler(saveID);
+            ID = saveID;
             Time = pointInTime;
             Save();
         }
-
+        /// <summary>
+        /// Saves the timeStamp to the saveHandler
+        /// </summary>
         public void Save() {
             saveHandler.Save(this);
         }
 
         public string ToBeSaved() {
             var savedTimeStamp = Convert.ToString(Time);
-            Debug.Log("Saved : " + savedTimeStamp);
+            Debug.Log($"Saved Timestamp {ID}: " + savedTimeStamp);
             return savedTimeStamp;
         }
 
         public void OnLoad(string value) {
-            Debug.Log("Loaded : " + value);
+            
             if (string.IsNullOrEmpty(value)) {
                 Time = epoch;
                 return;
             }
-
+            Debug.Log($"Loaded Timestamp {ID}: " + value);
             Time = Convert.ToDateTime(value);
         }
 
