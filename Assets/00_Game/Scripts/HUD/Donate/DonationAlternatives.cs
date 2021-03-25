f1267e6cc6d990dc11bfd5a07581151ddf2fece7
Eric S <eric@laurila.se>
2021-03-25 14:49:01 +0100

    makes it possible to choose an item to sacrifice

--- Assets/00_Game/Scripts/HUD/Donate/DonationAlternatives.cs
+++ Assets/00_Game/Scripts/HUD/Donate/DonationAlternatives.cs
@@ -1,15 +1,74 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Clothing;
+using Clothing.Inventory;
+using Clothing.Upgrade;
 using Currency.Coins;
+using HUD.Clothing;
 using UnityEngine;
+using UnityEngine.UI;
 using Utilities;
 
 namespace HUD.Donate {
     public class DonationAlternatives : MonoBehaviour{
-        DonationHandler donationHandler;
         CombinedWearables combinedWearables;
+        public Image slots;
         int[] donatedCoins = { -1000, -2000, -3000 };
+        PlayerInventory playerInventory;
+        DonationHandler donationHandler;
+
+        void Start() {
+            playerInventory = FindObjectOfType<PlayerInventory>();
+            donationHandler = FindObjectOfType<DonationHandler>();
+            foreach (var button in donationHandler.alternativesButtons) {
+                button.interactable = false;
+            }
+        }
+
+        void OnEnable() {
+            EventBroker.Instance().SubscribeMessage<EventAddToUpgradeSlot>(AssignUpCycleSlot);
+        }
+
+        void OnDisable() {
+            EventBroker.Instance().UnsubscribeMessage<EventAddToUpgradeSlot>(AssignUpCycleSlot);
+        }
+
+        public void AssignUpCycleSlot(EventAddToUpgradeSlot eventAddUpCycleClothes) {
+            AssignToSlot(eventAddUpCycleClothes.combinedWearable);
+        }
         
+        void AssignToSlot(CombinedWearables combinedWearables) {
+            donationHandler.qualifiesForDonation();
+            CheckIfValidForDonation(combinedWearables);
+            Debug.Log(donationHandler.isGood);
+            var instance = Instantiate(combinedWearables, slots.transform, true);
+                var scale = combinedWearables.GetComponent<RectTransform>().localScale;
+                instance.transform.localPosition = Vector2.zero;
+                instance.GetComponent<RectTransform>().localScale = scale;
+                Destroy(instance.GetComponent<AssignCombinedWearableToUpCycle>());
+                Destroy(instance.GetComponent<Button>());
+        }
+        
+        void CheckIfValidForDonation(CombinedWearables combinedWearables) {
+            if (donationHandler.isGood) {
+                for (var buttonToBeActive = 0; buttonToBeActive < donationHandler.stylePointsToUpgrade; buttonToBeActive++) {
+                    foreach (var button in donationHandler.alternativesButtons) {
+                        button.interactable = true;
+                    }
+                }
+            }
+            if (!(playerInventory.Amount(PlayerInventory.GetName(combinedWearables)) >= 2)) {
+                donationHandler.warningText.text = "This item does not have any duplicate yet. Come back later!";
+                Debug.Log("This item does not have any duplicate yet. Come back later!");
+            }
+            else if (!(donationHandler.CheckIfMaxStylePointsReached(combinedWearables.stylePoints, combinedWearables.rarity) >= 1)) {
+                donationHandler.warningText.text = "This item already has its maximum style points";
+                Debug.Log("This item already has its maximum style points");
+            }
+            else {
+                Debug.Log("Something went wrong..");
+            }
+        }
         
         public void DonateAlternative1() {
             donationHandler.DonateMeBaby(combinedWearables, 1);
--- Assets/00_Game/Scripts/HUD/Donate/DonationHandler.cs
+++ Assets/00_Game/Scripts/HUD/Donate/DonationHandler.cs
@@ -8,49 +8,36 @@ namespace HUD.Donate {
     public class DonationHandler : MonoBehaviour {
         CombinedWearables combinedWearables;
         PlayerInventory playerInventory;
-        int stylePointsToUpgrade;
+        [HideInInspector]
+        public int stylePointsToUpgrade;
         int addedStylePoints;
+        public bool isGood;
         
         public Button[] alternativesButtons;
         public Text warningText;
 
         void Start() {
             combinedWearables = GetComponent<CombinedWearables>();
-            playerInventory = FindObjectOfType<PlayerInventory>();
-            foreach (var button in alternativesButtons) {
-                button.interactable = false;
-            }
+            playerInventory = GetComponent<PlayerInventory>();
         }
-
-        /*void CheckIfValidForDonation() {
-            if (qualifiesForDonation) {
-                for (var buttonToBeActive = 0; buttonToBeActive < stylePointsToUpgrade; buttonToBeActive++) {
-                    foreach (var button in alternativesButtons) {
-                        button.interactable = true;
-                    }
-                }
-            }
-            if (!(playerInventory.combineWearablesAmount[combinedWearables] >= 2)) {
-                warningText.text = "This item does not have any duplicate yet. Come back later!";
-            }
-            else if (!(CheckIfMaxStylePointsReached(combinedWearables.stylePoints, combinedWearables.rarity) >= 1)) {
-                warningText.text = "This item already has its maximum style points";
-            }
-        }*/
-
         public void DonateMeBaby(CombinedWearables wearable, int stylePointsToAdd) {
             addedStylePoints = stylePointsToAdd;
             combinedWearables = wearable;
             EventBroker.Instance().SendMessage(new EventUpdatePlayerInventory(combinedWearables, -1));
             combinedWearables.stylePoints += addedStylePoints;
-            // Will need to be remade (only returns the old item with old stylepoints instead of the one with increased amount) 
             EventBroker.Instance().SendMessage(new EventUpdatePlayerInventory(combinedWearables, 1));
         }
-
-        /*bool qualifiesForDonation => CheckIfMaxStylePointsReached(combinedWearables.stylePoints, combinedWearables.rarity) >= 1 
-                       && playerInventory.combineWearablesAmount[combinedWearables] >= 2;*/
+        public void qualifiesForDonation() {
+            if (CheckIfMaxStylePointsReached(combinedWearables.stylePoints, combinedWearables.rarity) >= 1 && playerInventory.Amount(PlayerInventory.GetName(combinedWearables)) >= 2) {
+                isGood = true;
+            }
+            else {
+                Debug.Log("nothing man");
+                isGood = false;
+            }
+        }
         
-        int CheckIfMaxStylePointsReached(int currentPoints, Rarity rarity) {
+        public int CheckIfMaxStylePointsReached(int currentPoints, Rarity rarity) {
             stylePointsToUpgrade = rarity.MaxValue - currentPoints;
             return stylePointsToUpgrade;
         }
