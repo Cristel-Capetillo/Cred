ce3b371a383a5b32dd5b42bf4cf40e8028abd4c6
Michael Van Der Poll <michaelvdp@gmail.com>
2021-03-29 23:28:30 +0200

    Prevents MysteryBox menu from opening multiple times

--- Assets/00_Game/Scripts/HUD/MysteryBox/MysteryBoxButton.cs
+++ Assets/00_Game/Scripts/HUD/MysteryBox/MysteryBoxButton.cs
@@ -6,8 +6,11 @@ namespace HUD.MysteryBox {
         public RectTransform parentCanvas;
 
         public void SpawnLootBoxSelectionMenu() {
+            if (FindObjectOfType<SelectorMenu>()) {
+                Debug.Log("MysteryBox Selector Menu already open.");
+                return;
+            }
             var instance = Instantiate(selectorMenuPrefab, parentCanvas);
-            //instance.GetComponent<SelectorMenu>().GenerateMysteryBoxes(6);
         }
     }
 }
