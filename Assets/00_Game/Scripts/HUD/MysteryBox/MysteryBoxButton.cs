eacd4637f45a3c6baf930089ffda8f6bd99e81fc
Michael Van Der Poll <michaelvdp@gmail.com>
2021-03-30 00:22:19 +0200

    Dev cheats for Coins and LootBoxes
    F7 and F11 (only works in UnityEditor)

--- Assets/00_Game/Scripts/HUD/MysteryBox/MysteryBoxButton.cs
+++ Assets/00_Game/Scripts/HUD/MysteryBox/MysteryBoxButton.cs
@@ -1,4 +1,8 @@
+using System;
+using Currency.Coins;
+using MysteryBox;
 using UnityEngine;
+using Utilities;
 
 namespace HUD.MysteryBox {
     public class MysteryBoxButton : MonoBehaviour {
@@ -11,6 +15,18 @@ namespace HUD.MysteryBox {
                 return;
             }
             var instance = Instantiate(selectorMenuPrefab, parentCanvas);
+        }
+        
+        void Update() {
+#if UNITY_EDITOR
+            if (Input.GetKeyDown(KeyCode.F11)) {
+                EventBroker.Instance().SendMessage(new EventUpdateCoins(300));
+            }
+
+            if (Input.GetKeyDown(KeyCode.F7)) {
+                EventBroker.Instance().SendMessage(new EventMysteryBoxBought());
+            }
+#endif
         }
     }
 }
