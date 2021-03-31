93735cef3d99d818adf89a75176a4e287bffbf82
Adam Tapper <adam@tapper.one>
2021-03-31 10:57:11 +0200

    no message

---
+++ Assets/00_Game/Scripts/Clothing/Inventory/Hud/ShowSpriteInBuyMenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using Clothing.Inventory;
+using Clothing.Upgrade.UpCycle;
+using UnityEngine;
+using UnityEngine.UI;
+using Utilities;
+namespace HUD.MysteryBox {
+    public class ShowSpriteInBuyMenu : MonoBehaviour {
+        
+    }
+}
