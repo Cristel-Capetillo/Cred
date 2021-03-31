c1fb063ed234eba3187c1ed7f966bc5f68412b56
Thomas Ahlström <thomas.ahlstrom@student.forsbergsskola.se>
2021-04-01 00:00:40 +0200

    Adds ClubSuccess.cs

--- Assets/00_Game/Scripts/ClientMissions/Hud/ClubSuccess.cs
+++ Assets/00_Game/Scripts/ClientMissions/Hud/ClubSuccess.cs
@@ -1,10 +1,10 @@
+using System.Collections;
 using ClientMissions.Messages;
 using Clothing.DressUp;
 using Currency.Coins;
-using UnityEngine; 
+using UnityEngine;
 using UnityEngine.UI;
 using Utilities;
-using System.Collections;
 
 namespace ClientMissions.Hud {
     public class ClubSuccess : MonoBehaviour {
@@ -13,16 +13,19 @@ namespace ClientMissions.Hud {
         [SerializeField] Button collectButton;
         [SerializeField] Button mainMenuButton;
 
+        [SerializeField, Header("Set the amount of coins to be displayed when pressing collect button")]
+        float animatedResourceDivider = 10f;
+        [SerializeField, Header("Set the speed of coins in seconds to be displayed when pressing collect button")]
+        float secondsBetweenResource = .05f;
+
+        public GameObject coinSprite;
+        public GameObject followerSprite;
+        public Transform coinUITransform;
+        public Transform followerUITransform;
+
         int currencyReward;
         int followersReward;
-        
-        [SerializeField] [Tooltip("Set the amount of coins to be displayed when pressing collect button")]
-        float animatedCoinsDivider = 10f;
-        public float secondsBetweenCoins = .05f;
-        public GameObject coinItem;
-        public Transform coinFeatures;
 
-      
         void Start() {
             EventBroker.Instance().SubscribeMessage<ShowRewardMessage>(ShowReward);
         }
@@ -30,6 +33,7 @@ namespace ClientMissions.Hud {
         void OnDestroy() {
             EventBroker.Instance().UnsubscribeMessage<ShowRewardMessage>(ShowReward);
         }
+
         void ShowReward(ShowRewardMessage rewardMessage) {
             GetComponent<Canvas>().enabled = true;
             currencyReward = rewardMessage.CurrencyReward;
@@ -37,23 +41,39 @@ namespace ClientMissions.Hud {
             rewardText.text = rewardMessage.CurrencyReward.ToString();
             followersRewardText.text = rewardMessage.FollowersReward.ToString();
         }
+
+        // void ShowReward(ShowRewardMessage rewardMessage) {
+        //     GetComponent<Canvas>().enabled = true;
+        //     currencyReward = rewardMessage.CurrencyReward;
+        //     followersReward = rewardMessage.FollowersReward;
+        //     //TODO: Currency and Followers!
+        //     rewardText.text = rewardMessage.CurrencyReward.ToString();
+        //     followersRewardText.text = rewardMessage.FollowersReward.ToString();
+        // }
+
         public void CollectReward() {
             EventBroker.Instance().SendMessage(new EventUpdateCoins(currencyReward));
             EventBroker.Instance().SendMessage(new UpdateFollowersMessage(followersReward));
             EventBroker.Instance().SendMessage(new RemoveAllClothes());
             mainMenuButton.gameObject.SetActive(true);
             collectButton.gameObject.SetActive(false);
-            StartCoroutine(SpawnCoins(currencyReward/animatedCoinsDivider));
+            StartCoroutine(SpawnCoins(currencyReward / animatedResourceDivider));
+            StartCoroutine(SpawnFollowers(currencyReward / animatedResourceDivider));
         }
-        
 
-        IEnumerator SpawnCoins(float quantity)
-        {
-            for(int i = 0; i < quantity; i++)
-            {
-                GameObject go = Instantiate(coinItem, coinFeatures);
-                go.transform.SetParent(coinFeatures);
-                yield return new WaitForSeconds(secondsBetweenCoins);
+        IEnumerator SpawnCoins(float quantity) {
+            for (var i = 0; i < quantity; i++) {
+                var go = Instantiate(coinSprite, coinUITransform);
+                go.transform.SetParent(coinUITransform);
+                yield return new WaitForSeconds(secondsBetweenResource);
+            }
+        }
+
+        IEnumerator SpawnFollowers(float quantity) {
+            for (var i = 0; i < quantity; i++) {
+                var go = Instantiate(followerSprite, followerUITransform);
+                go.transform.SetParent(coinUITransform);
+                yield return new WaitForSeconds(secondsBetweenResource);
             }
         }
     }
