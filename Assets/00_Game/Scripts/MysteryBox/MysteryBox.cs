b6d835138fa2435a85d2c6d04ca42c32973a6d86
Alex <Alex.rynkowski90@gmail.com>
2021-03-31 14:30:45 +0200

    fix mystery box amount bug

--- Assets/00_Game/Scripts/MysteryBox/MysteryBox.cs
+++ Assets/00_Game/Scripts/MysteryBox/MysteryBox.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using Clothing;
 using Clothing.Inventory;
 using UnityEngine;
@@ -7,7 +8,6 @@ using Utilities;
 
 namespace MysteryBox {
     public class MysteryBox : MonoBehaviour {
-        
         [SerializeField] float spawnRewardAfterDelay = 1.5f;
         [SerializeField] float destroyDelay = 2f;
 
@@ -24,11 +24,12 @@ namespace MysteryBox {
         IEnumerator StartRewardProcess(float delay) {
             yield return new WaitForSeconds(delay);
             var reward = lootTable.Reward();
+            reward.Amount = Convert.ToInt32(FindObjectOfType<PlayerInventory>().temporaryData[reward.ToString()][InventoryData.Amount]);
             ShowReward(reward);
             EventBroker.Instance().SendMessage(new EventUpdatePlayerInventory(reward, 1));
             Destroy(gameObject, destroyDelay);
         }
-        
+
         void ShowReward(CombinedWearables reward) {
             EventBroker.Instance().SendMessage(new EventMysteryBoxOpened(reward));
             EventBroker.Instance().SendMessage(new EventShowReward(reward));
