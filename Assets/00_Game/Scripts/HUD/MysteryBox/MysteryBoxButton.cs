8c6bd87bb22ece2b9c5ade78b4a36eb2e670e6a9
Michael Van Der Poll <michaelvdp@gmail.com>
2021-03-31 16:42:54 +0200

    MysteryBox shows quantity

--- Assets/00_Game/Scripts/HUD/MysteryBox/MysteryBoxButton.cs
+++ Assets/00_Game/Scripts/HUD/MysteryBox/MysteryBoxButton.cs
@@ -1,13 +1,33 @@
-using System;
+using System.Collections;
 using Currency.Coins;
 using MysteryBox;
 using UnityEngine;
+using UnityEngine.UI;
 using Utilities;
 
 namespace HUD.MysteryBox {
     public class MysteryBoxButton : MonoBehaviour {
         public GameObject selectorMenuPrefab;
+        public Text amountText;
         public RectTransform parentCanvas;
+
+        IEnumerator Start() {
+            yield return new WaitForSeconds(3f);
+            var amount = FindObjectOfType<MysteryBoxInventory>().Owned;
+            amountText.text = amount > 0 ? $"{amount}" : " ";
+            EventBroker.Instance().SubscribeMessage<EventMysteryBoxBought>(OnLootBoxPurchased);
+            EventBroker.Instance().SubscribeMessage<EventMysteryBoxOpened>(OnLootBoxOpened);
+        }
+
+        void OnLootBoxOpened(EventMysteryBoxOpened eventMysteryBoxOpened) => UpdateLootBoxAmount();
+
+        void OnLootBoxPurchased(EventMysteryBoxBought eventMysteryBoxBought) => UpdateLootBoxAmount();
+        
+
+        void UpdateLootBoxAmount() {
+            var amount = FindObjectOfType<MysteryBoxInventory>().Owned;
+            amountText.text = amount > 0 ? $"{amount}" : " ";
+        }
 
         public void SpawnLootBoxSelectionMenu() {
             if (FindObjectOfType<SelectorMenu>()) {
