37ed909c0d2055288bd9b7dce548bf701da2bdf8
Alex <Alex.rynkowski90@gmail.com>
2021-03-31 18:34:36 +0200

    implements club variations (FMOD)

--- Assets/00_Game/Scripts/ClientMissions/Hud/RequirementsUI.cs
+++ Assets/00_Game/Scripts/ClientMissions/Hud/RequirementsUI.cs
@@ -6,52 +6,60 @@ using TMPro;
 using UnityEngine;
 using Utilities;
 
-namespace ClientMissions.Hud{
-    public class RequirementsUI : MonoBehaviour{
+namespace ClientMissions.Hud {
+    public class RequirementsUI : MonoBehaviour {
         [SerializeField] TMP_Text requirementHeader;
         [SerializeField] TMP_Text stylePoints;
         [SerializeField] TMP_Text requirementTMPTextPrefab;
         [SerializeField] Transform requirementTextParent;
-        Dictionary<string,TMP_Text> requirements = new Dictionary<string, TMP_Text>();
+        Dictionary<string, TMP_Text> requirements = new Dictionary<string, TMP_Text>();
         [SerializeField] List<GameObject> clientGameObjects = new List<GameObject>();
         [SerializeField] MeshRenderer clientSkin;
         string requiredStylepoints;
-        
-        void Awake(){
+
+
+        void Awake() {
             EventBroker.Instance().SubscribeMessage<SendActiveMissionMessage>(OnGetMissionData);
         }
-        void OnDestroy(){
+
+        void OnDestroy() {
             EventBroker.Instance().UnsubscribeMessage<RequirementUIMessage>(UpdateRequirementUI);
             EventBroker.Instance().UnsubscribeMessage<CurrentStylePointsMessage>(UpdateStylePointsUI);
             EventBroker.Instance().UnsubscribeMessage<SendActiveMissionMessage>(OnGetMissionData);
         }
-        void OnGetMissionData(SendActiveMissionMessage sendActiveMissionMessage){
+
+        void OnGetMissionData(SendActiveMissionMessage sendActiveMissionMessage) {
             var missionData = sendActiveMissionMessage.MissionData;
-            if (missionData.ClientData == null){
+            if (missionData.ClientData == null) {
                 Debug.LogWarning("RequirementUI missionData is null");
                 return;
             }
+
             requiredStylepoints = $"/{missionData.StylePointValues.MinStylePoints.ToString()}";
             requirementHeader.text = $"{missionData.ClientData.name}s requirements:";
             stylePoints.text = $"0{requiredStylepoints}";
-            foreach (var client in clientGameObjects){
+            foreach (var client in clientGameObjects) {
                 client.SetActive(client.name == missionData.ClientData.name);
             }
+
             clientSkin.material = missionData.ClientData.Skin;
-            foreach (var requirement in missionData.Requirements){
+            foreach (var requirement in missionData.Requirements) {
                 var temp = Instantiate(requirementTMPTextPrefab, requirementTextParent);
                 temp.text = requirement.ToString();
-                requirements.Add(requirement.ToString(),temp);
+                requirements.Add(requirement.ToString(), temp);
             }
+
             EventBroker.Instance().SubscribeMessage<RequirementUIMessage>(UpdateRequirementUI);
             EventBroker.Instance().SubscribeMessage<CurrentStylePointsMessage>(UpdateStylePointsUI);
             EventBroker.Instance().UnsubscribeMessage<SendActiveMissionMessage>(OnGetMissionData);
         }
-        void UpdateStylePointsUI(CurrentStylePointsMessage currentStylePoints){
+
+        void UpdateStylePointsUI(CurrentStylePointsMessage currentStylePoints) {
             stylePoints.text = $"{currentStylePoints.CurrentStylePoints}{requiredStylepoints}";
         }
-        void UpdateRequirementUI(RequirementUIMessage requirementUIMessage){
-            if (requirements.ContainsKey(requirementUIMessage.RequirementName)){
+
+        void UpdateRequirementUI(RequirementUIMessage requirementUIMessage) {
+            if (requirements.ContainsKey(requirementUIMessage.RequirementName)) {
                 requirements[requirementUIMessage.RequirementName].fontStyle = requirementUIMessage.IsCompleted
                     ? FontStyles.Strikethrough
                     : FontStyles.Normal;
